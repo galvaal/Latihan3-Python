c489c5388ad27b5566f6b96c278d6ed68cd53396
galvaal <115516730+galvaal@users.noreply.github.com>
2022-10-30 19:10:04 +0700

    Add files via upload

---
+++ luas dan keliling lingkaran.py
@@ -0,0 +1,8 @@
+print('Menghitung luas dan keliling lingakaran')
+print('==============================')
+r = int(input('masukan jari-jari lingkaran: '))
+phi = 3.14
+L = phi * (r * r)
+K = 2 * phi * r
+print('Luas lingkaran dengan jari-jari {} adalah {}'.format(r, L))
+print('Keliling lingkaran dengan jari-jari {} adalah {}'.format(r, K))
